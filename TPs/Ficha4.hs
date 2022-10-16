37ff4e0164afc3ffb594ab0995613e0118d30d0e
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-10-16 10:10:30 +0100

    Ficha 4 completa

--- TPs/Ficha4.hs
+++ TPs/Ficha4.hs
@@ -26,4 +26,54 @@ mapArv _ Vazia = Vazia
 mapArv f (No x l r) = No (f x) (mapArv f l) (mapArv f r)
 
 --4.5
+--a)
+construir :: Ord a =>  [a] -> Arv a
+construir [] = Vazia
+construir [x] = No x Vazia Vazia
+construir xs = No elementoMeio (construir primeiraMetade) (construir segundaMetade)
+    where 
+        meio = length xs `div` 2
+        elementoMeio = xs !! meio
+        primeiraMetade = take meio xs
+        segundaMetade = drop (meio+1) xs
 
+--b)
+inserir :: Ord a =>  a -> Arv a -> Arv a
+inserir x Vazia = No x Vazia Vazia
+inserir x (No y left right) 
+    | x == y = No y left right
+    | x < y = No y (inserir x left) right
+    | otherwise = No y left (inserir x right)
+
+construirLinear :: Ord a => [a] -> Arv a
+construirLinear = foldr inserir Vazia
+
+altura :: Arv a -> Int
+altura Vazia = 0
+altura (No _ Vazia Vazia) = 0
+altura (No _ left right) = 1 + max(altura left) (altura right)
+
+testHeight :: ([Int] -> Arv Int) -> [Int] -> [Int]
+testHeight constructTree xs = [(altura . constructTree)[1..x] | x <-xs]
+
+--4.6
+--a)
+maisDir :: Arv a -> a
+maisDir Vazia = error "Arvore vazia :/ "
+maisDir (No x _ Vazia) = x
+maisDir (No _ _ right) = maisDir right
+
+-- b)
+remover :: Ord a => a -> Arv a -> Arv a
+remover _ Vazia = Vazia
+remover x (No y left right) 
+    | x < y = No y (remover x left) right
+    | x > y = No y left (remover x right)
+    | otherwise = removerNo (No y left right)
+
+removerNo :: Ord a=> Arv a  -> Arv a
+removerNo Vazia = error "Nao tem nos para remover"
+removerNo (No _ Vazia right) = right
+removerNo (No _  left Vazia) = left
+removerNo (No _  left right) = No n (remover n left) right
+    where n = maisDir left
