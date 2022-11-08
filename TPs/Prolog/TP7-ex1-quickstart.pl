988d006606da6c3c23495e207909f3c9760ea4bd
Jorge <jotac431@gmail.com>
2022-11-08 12:29:29 +0000

    prolog

--- TPs/Prolog/TP7-ex1-quickstart.pl
+++ TPs/Prolog/TP7-ex1-quickstart.pl
@@ -37,3 +37,78 @@ parent(barb,pameron).
 parent(merle,pameron).
 
 % ... The next facts for parent/2 correspond to the children of phil and claire
+
+parent(phil, haley).
+parent(phil, alex).
+parent(phil, luke).
+parent(claire, haley).
+parent(claire, alex).
+parent(claire, luke).
+
+parent(mitchell, lily).
+parent(mitchell, rexford).
+parent(cameron, lily).
+parent(cameron, rexford).
+
+parent(pameron, calhoun).
+parent(bo, calhoun).
+
+parent(dylan, george).
+parent(haley, george).
+parent(dylan, poppy).
+parent(haley, poppy).
+
+% c)
+father(X,Y):-
+    male(X),
+    parent(X,Y).
+
+grandparent(X,Y):-
+    parent(Parent,Y),
+    parent(X,Parent).
+
+grandmother(X,Y):-
+    female(X),
+    grandparent(X,Y).
+
+siblings(X,Y):-
+    parent(A,X),
+    parent(A,Y),
+    parent(Z,X),
+    parent(Z,Y),
+    X\=Y,
+    A@<Z.
+
+%...
+
+% e)
+
+%married(Person1,Person2,Year).
+married(jay,gloria,2008).
+married(jay,dede,1968).
+
+divorced(jay,dede,2003).
+
+%is_married(+X,?Y,+Year)
+is_married(X,Y,Year):-
+    married(X,Y,Year1),
+    Year >= Year1,
+    \+divorced(X,Y,_).
+
+is_married(X,Y,Year):-
+    married(X,Y,Year1),
+    Year >= Year1,
+    divorced(X,Y,Year2),
+    Year =< Year2.
+
+% 6.
+% a)
+
+pairs(X, Y):- d(X), q(Y).
+pairs(X, X):- u(X).
+u(1).
+d(2).
+d(4).
+q(4).
+q(16).
+
